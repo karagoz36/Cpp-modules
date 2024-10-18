508559eead86d8adbd28216fede072c77b537dd6
Taha Karagoz <tkaragoz@student.42.fr>
2024-10-18 16:19:46 +0200

    all done

--- cpp-01/ex01/main.cpp
+++ cpp-01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkaragoz <tkaragoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 15:20:47 by tkaragoz          #+#    #+#             */
-/*   Updated: 2024/10/18 15:23:42 by tkaragoz         ###   ########.fr       */
+/*   Updated: 2024/10/18 16:19:24 by tkaragoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 Zombie* zombieHorde(int N, std::string name);
 
-int main() {
+int main(void) {
 
 	int N = 5;
-
 	Zombie* horde = zombieHorde(N, "zombieHorde");
+
 	for (int i = 0; i < N; i++){
 		horde->announce();
 	}
 	delete[] horde;
+
 	return (0);
 }
